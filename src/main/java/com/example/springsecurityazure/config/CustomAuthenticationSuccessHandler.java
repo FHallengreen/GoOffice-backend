cb639f38ad5bdd95f41f9c486df63e4098d43a7c
Frederik Hallengreen <98979230+FHallengreen@users.noreply.github.com>
2023-05-22 13:35:36 +0200

    Update CustomAuthenticationSuccessHandler.java

--- src/main/java/com/example/springsecurityazure/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/springsecurityazure/config/CustomAuthenticationSuccessHandler.java
@@ -49,7 +49,7 @@ public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthent
         EmployeeRequest employeeRequest = new EmployeeRequest(email,name,profilePic);
         employeeService.createEmployeeFromGoogleAuth(employeeRequest);
 
-        String targetUrl = "https://thankful-coast-0e5d88710.3.azurestaticapps.net/#/";
+        String targetUrl = "https://polite-cliff-027946e03.3.azurestaticapps.net/#/";
         redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 }
